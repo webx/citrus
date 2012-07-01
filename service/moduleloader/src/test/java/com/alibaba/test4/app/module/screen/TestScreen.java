90747ceef53f02e4a03664b8b26e34acffc97db5
cqsuper <cqsuper@gmail.com>
2012-07-01 12:25:07 +0800

    修改screen里方法的映射规则，由之前的public方法变成通过annotation来标识此方法可用来映射页面请求。 另外修改增加了单元测试。

---
+++ service/moduleloader/src/test/java/com/alibaba/test4/app/module/screen/TestScreen.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.alibaba.test4.app.module.screen;
+
+import com.alibaba.citrus.service.moduleloader.bind.annotation.RequestMapping;
+
+/**
+ * @author qianchao
+ * 
+ */
+public class TestScreen {
+
+	public void execute() {
+		
+	}
+
+	@RequestMapping
+	public void hello() {
+
+	}
+
+}
