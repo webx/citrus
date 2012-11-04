0aa687fd456c36edc7c71e8e33fa96eb78c89768
Michael Zhou <yizhi@taobao.com>
2012-11-03 18:32:03 -0600

    增加了一个新的基类，以方便valve之间传递数据。

---
+++ webx/turbine/src/main/java/com/alibaba/citrus/turbine/pipeline/valve/AbstractInOutValve.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright (c) 2002-2012 Alibaba Group Holding Limited.
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alibaba.citrus.turbine.pipeline.valve;
+
+import static com.alibaba.citrus.util.StringUtil.*;
+
+import com.alibaba.citrus.service.pipeline.PipelineContext;
+import com.alibaba.citrus.service.pipeline.support.AbstractValve;
+
+/**
+ * 从该抽象基类派生的valve，支持从pipeline context中读取、或者向pipeline context输出一个对象。
+ * 例如，<code>PerformScreenValve</code>可以输出一个对象，而<code>RenderResultAsJsonValve</code>则取得该对象并生成JSON。
+ *
+ * @author Michael Zhou
+ */
+public abstract class AbstractInOutValve extends AbstractValve {
+    protected static final String DEFAULT_ATTRIBUTE_KEY = "result";
+    private String inKey;
+    private String outKey;
+
+    public final String getIn() {
+        String key = inKey;
+
+        if (key == null) {
+            key = getInDefault();
+        }
+
+        if (key == null) {
+            key = DEFAULT_ATTRIBUTE_KEY;
+        }
+
+        return key;
+    }
+
+    /**
+     * 子类可覆盖此方法，以设置in的默认值。
+     *
+     * @return 返回<code>null</code>使用系统默认值。
+     */
+    protected String getInDefault() {
+        return null;
+    }
+
+    public final void setIn(String inKey) {
+        this.inKey = trimToNull(inKey);
+    }
+
+    public final String getOut() {
+        String key = outKey;
+
+        if (key == null) {
+            key = getOutDefault();
+        }
+
+        if (key == null) {
+            key = DEFAULT_ATTRIBUTE_KEY;
+        }
+
+        return key;
+    }
+
+    /**
+     * 子类可覆盖此方法，以设置out的默认值。
+     *
+     * @return 返回<code>null</code>使用系统默认值。
+     */
+    protected String getOutDefault() {
+        return null;
+    }
+
+    public final void setOut(String outKey) {
+        this.outKey = trimToNull(outKey);
+    }
+
+    /** 从pipeline context中取得输入对象。 */
+    protected final Object getInputValue(PipelineContext pipelineContext) {
+        return pipelineContext.getAttribute(getIn());
+    }
+
+    /**
+     * 从pipeline context中取出输入对象。
+     * 假如对象是可被接受的，那么该对象将从context中被删除，也就是说，没有第二个人可以再次取得同一个对象。
+     *
+     * @return 如果对象存在且被<code>filterInputValue(Object)</code>所接受，则返回之；否则返回<code>null</code>。
+     */
+    protected final Object consumeInputValue(PipelineContext pipelineContext) {
+        Object value = pipelineContext.getAttribute(getIn());
+
+        if (value != null && filterInputValue(value)) {
+            pipelineContext.setAttribute(getIn(), null);
+        } else {
+            value = null;
+        }
+
+        return value;
+    }
+
+    /**
+     * 子类可以覆盖此方法，以接受特定的对象。
+     *
+     * @return 如果对象可被接受，则返回<code>true</code>。默认的实现总是返回<code>true</code>。
+     */
+    protected boolean filterInputValue(Object inputValue) {
+        return true;
+    }
+
+    /** 将指定的对象放到pipeline context中，作为输出对象。 */
+    protected final void setOutputValue(PipelineContext pipelineContext, Object outputValue) {
+        pipelineContext.setAttribute(getOut(), outputValue);
+    }
+}
---
+++ webx/turbine/src/test/java/com/alibaba/citrus/turbine/pipeline/valve/InOutValveTests.java
@@ -0,0 +1,213 @@
+/*
+ * Copyright (c) 2002-2012 Alibaba Group Holding Limited.
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alibaba.citrus.turbine.pipeline.valve;
+
+import static com.alibaba.citrus.util.CollectionUtil.*;
+import static org.junit.Assert.*;
+
+import java.util.Map;
+
+import com.alibaba.citrus.service.pipeline.PipelineContext;
+import com.alibaba.citrus.util.internal.InterfaceImplementorBuilder;
+import org.junit.Before;
+import org.junit.Test;
+
+public class InOutValveTests {
+    private AbstractInOutValve valve;
+    private PipelineContext    context;
+    private Map<String, Object> attrs = createHashMap();
+
+    @Before
+    public void init() {
+        valve = new MyInOutValve();
+        context = (PipelineContext) new InterfaceImplementorBuilder().addInterface(PipelineContext.class).setOverrider(new Object() {
+            public Object getAttribute(String key) {
+                return attrs.get(key);
+            }
+
+            public void setAttribute(String key, Object value) {
+                attrs.put(key, value);
+            }
+        }).toObject();
+    }
+
+    @Test
+    public void in_systemDefaultValue() {
+        assertEquals("result", valve.getIn());
+
+        valve = new MyInOutValve() {
+            @Override
+            protected String getInDefault() {
+                return null; // 返回null表示使用系统默认值
+            }
+        };
+
+        assertEquals("result", valve.getIn());
+
+        // 取消默认值
+        valve.setIn("specified");
+        assertEquals("specified", valve.getIn());
+    }
+
+    @Test
+    public void in_defaultValue() {
+        valve = new MyInOutValve() {
+            @Override
+            protected String getInDefault() {
+                return "myin";
+            }
+        };
+
+        assertEquals("myin", valve.getIn());
+
+        // 取消默认值
+        valve.setIn("specified");
+        assertEquals("specified", valve.getIn());
+    }
+
+    @Test
+    public void in_set() {
+        valve.setIn("  ");
+        assertEquals("result", valve.getIn());
+
+        valve.setIn(" myresult ");
+        assertEquals("myresult", valve.getIn());
+    }
+
+    @Test
+    public void out_systemDefaultValue() {
+        assertEquals("result", valve.getOut());
+
+        valve = new MyInOutValve() {
+            @Override
+            protected String getOutDefault() {
+                return null; // 返回null表示使用系统默认值
+            }
+        };
+
+        assertEquals("result", valve.getOut());
+
+        // 取消默认值
+        valve.setOut("specified");
+        assertEquals("specified", valve.getOut());
+    }
+
+    @Test
+    public void out_defaultValue() {
+        valve = new MyInOutValve() {
+            @Override
+            protected String getOutDefault() {
+                return "myout";
+            }
+        };
+
+        assertEquals("myout", valve.getOut());
+
+        // 取消默认值
+        valve.setOut("specified");
+        assertEquals("specified", valve.getOut());
+    }
+
+    @Test
+    public void out_set() {
+        valve.setOut("  ");
+        assertEquals("result", valve.getOut());
+
+        valve.setOut(" myresult ");
+        assertEquals("myresult", valve.getOut());
+    }
+
+    @Test
+    public void getInputValue() {
+        assertNull(valve.getInputValue(context));
+
+        attrs.put("result", "hello");
+        assertEquals("hello", valve.getInputValue(context));
+
+        assertEquals("hello", valve.getInputValue(context));
+    }
+
+    @Test
+    public void consumeInputValue() {
+        assertNull(valve.getInputValue(context));
+
+        attrs.put("result", "hello");
+        assertEquals("hello", valve.consumeInputValue(context));
+
+        assertNull(valve.getInputValue(context));
+    }
+
+    @Test
+    public void consumeInputValue_withFilter() {
+        valve = new MyInOutValve() {
+            @Override
+            protected boolean filterInputValue(Object inputValue) {
+                return inputValue instanceof String;
+            }
+        };
+
+        assertNull(valve.getInputValue(context));
+
+        // filter方法只接受string类型
+        attrs.put("result", "hello");
+        assertEquals("hello", valve.consumeInputValue(context)); // 对于支持的类型，返回对象
+        assertNull(valve.getInputValue(context)); // 并从context中删除该对象
+
+        // 如果是其它类型，则忽略之
+        Object obj = new Object();
+        attrs.put("result", obj);
+        assertSame(obj, valve.getInputValue(context));
+        assertSame(null, valve.consumeInputValue(context)); // 对不支持的类型，返回null
+        assertSame(obj, valve.getInputValue(context)); // 然后原对象还在context中
+    }
+
+    @Test
+    public void in_out_different() {
+        valve = new MyInOutValve() {
+            @Override
+            protected String getInDefault() {
+                return "myin";
+            }
+
+            @Override
+            protected String getOutDefault() {
+                return "myout";
+            }
+        };
+
+        attrs.put("myin", "hello");
+        assertEquals("hello", valve.getInputValue(context));
+
+        valve.setOutputValue(context, "hi");
+        assertEquals("hello", attrs.get("myin"));
+        assertEquals("hi", attrs.get("myout"));
+    }
+
+    @Test
+    public void setOutputValue() {
+        assertNull(valve.getInputValue(context));
+
+        valve.setOutputValue(context, "hi");
+        assertEquals("hi", valve.getInputValue(context));
+    }
+
+    public static class MyInOutValve extends AbstractInOutValve {
+        public void invoke(PipelineContext pipelineContext) throws Exception {
+        }
+    }
+}
